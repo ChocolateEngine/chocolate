836c0da53963e04ad2a87528eafd4110dba1998c
Demez <demizexp@gmail.com>
2021-09-18 22:25:28 -0400

    add keystate to input system

--- inc/core/input.h
+++ inc/core/input.h
@@ -19,15 +19,19 @@ public:
 	std::string	aCmd;
 };
 
+
 class InputSystem : public BaseInputSystem
 {
 protected:
 	typedef std::vector< KeyAlias > KeyAliases;
 	typedef std::vector< KeyBind >	KeyBinds;
+	typedef std::unordered_map< SDL_Scancode, KeyState >	KeyStates;
 	
 	SDL_Event 	aEvent;
 	KeyAliases 	aKeyAliases;
 	KeyBinds 	aKeyBinds;
+	KeyStates 	aKeyStates;
+	const Uint8*      aKeyboardState;
 
 	/* Parse the key aliases that can be bound to.  */
 	void 		MakeAliases(  );
@@ -38,7 +42,13 @@ protected:
 	/* Initializes all console commands the system can respond to.  */
 	void 		InitConsoleCommands(  );
 	/*   */
+	void 		Init(  );
+	/*   */
 	void 		Update( float frameTime );
+	/* Update all key states on this frame  */
+	void 		UpdateKeyStates(  );
+	/* Update all key states on this frame  */
+	void 		UpdateKeyState( SDL_Scancode key );
 
 	void        ResetInputs(  );
 
@@ -53,9 +63,21 @@ public:
 	const glm::vec2& GetMouseDelta(  );
 	const glm::vec2& GetMousePos(  );
 
-	bool WindowHasFocus(  );
+	/* Is the engine window in focus?.  */
+	bool        WindowHasFocus(  );
 
-	/* Cosntructor.  */
+	/* Add a Key to the key update list.  */
+	void        RegisterKey( SDL_Scancode key );
+
+	/* Get the current KeyState value.  */
+	KeyState    GetKeyState( SDL_Scancode key );
+
+	/* Convienence functions  */
+	bool IsKeyPressed( SDL_Scancode key ) { return GetKeyState(key) & KeyState_Pressed; }
+	bool IsKeyJustPressed( SDL_Scancode key ) { return GetKeyState(key) & KeyState_JustPressed; }
+	bool IsKeyJustReleased( SDL_Scancode key ) { return GetKeyState(key) & KeyState_JustReleased; }
+
+	/* Constructor.  */
 	explicit        InputSystem(  );
 };
 
