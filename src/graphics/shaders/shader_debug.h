92d7cbc8fe0632462ea4a5366c855ce9a06c606f
Demez <demizexp@gmail.com>
2022-01-28 20:21:51 -0500

    forgot to push debug shader cpp/h

---
+++ src/graphics/shaders/shader_debug.h
@@ -0,0 +1,38 @@
+/*
+shader_debug.h ( Authored by Demez )
+
+The Debug Shader, used for wireframe
+*/
+#pragma once
+
+#include "shaders.h"
+
+class ShaderDebug: public BaseShader
+{
+protected:
+
+
+public:	
+	                    ShaderDebug(): BaseShader() {}
+
+	virtual void        Init() override;
+	virtual void        ReInit() override;
+
+	virtual std::vector<VkDescriptorSetLayoutBinding> GetDescriptorSetLayoutBindings(  ) override;
+
+	virtual void        CreateGraphicsPipeline(  ) override;
+
+	virtual void        UpdateBuffers( uint32_t sCurrentImage, BaseRenderable* spRenderable ) override;
+
+	virtual void        Draw( BaseRenderable* renderable, VkCommandBuffer c, uint32_t commandBufferIndex ) override;
+
+	//inline bool         UsesUniformBuffers(  ) override { return true; };
+};
+
+
+struct DebugPushConstant
+{
+	alignas(16) glm::mat4 aTransform;
+};
+
+
