20ea68657a060d9b5430b2f10597f9c67757421f
Demez <demizexp@gmail.com>
2022-06-20 17:15:58 -0400

    gui: remove AssignWindow func

--- public/igui.h
+++ public/igui.h
@@ -15,16 +15,17 @@ public:
 	// virtual ImFont* 	BuildFont( const char* spPath, float sSizePixels = 15.f, const ImFontConfig* spFontConfig = nullptr , const ImWchar* spGlyphRanges = nullptr ) = 0;
 	virtual ImFont* 	BuildFont( const char* spPath, float sSizePixels = 15.f, const ImFontConfig* spFontConfig = nullptr ) = 0;
 
-	/* Gets the window pointer from the renderer.  */
-	virtual void 		AssignWindow( SDL_Window* spWindow ) = 0;
 	/* Shows the console window.  */
 	virtual void		ShowConsole(  ) = 0;
+	
 	/* Is the console shown.  */
 	virtual bool		IsConsoleShown(  ) = 0;
+	
 	/* Debug Text on the side of the screen  */
 	virtual void		DebugMessage( const char* format, ... ) = 0;
 	virtual void		DebugMessage( size_t index, const char* format, ... ) = 0;
 	virtual void		InsertDebugMessage( size_t index, const char* format, ... ) = 0;
+	
 	/*
 	 *    Starts a new ImGui frame.
 	 */	
